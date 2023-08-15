6afc9be2bd5553a553ba238a7a126ddadff00379
shivanee <shiva.shivanee9@gmail.com>
2023-08-15 16:38:28 +0530

    Adding post and put api calls

--- src/main/java/com/project/kanban/springboot/springbootbackend/TodoResource.java
+++ src/main/java/com/project/kanban/springboot/springbootbackend/TodoResource.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +33,17 @@ public class TodoResource {
 	public ResponseEntity<Void> deleteTodo(@PathVariable String status,@PathVariable int id) {
 		todoService.deleteById(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{status}/todos/{id}")
+	public Todo updateTodo(@PathVariable String status,@PathVariable int id, @RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return todo;
+	}
+	
+	@PostMapping("/{status}/todos")
+	public Todo createTodo(@PathVariable String status, @RequestBody Todo todo) {
+		todoService.addTodo(todo.getTitle(),status);
+		return todo;
 	}
 }
